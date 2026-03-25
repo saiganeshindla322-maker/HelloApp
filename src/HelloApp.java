4307cb9877ee30b3659ac378b1affc25cc31682d
Sai Ganesh <saiganeshindla322@gmail.com>
2026-03-26 02:35:45 +0530

    Refactor main method to simplify greeting

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,9 +1,6 @@
 public class HelloApp {
-    public static void main(String[] args) {
-        // Get the name from command-line argument
-        String name = args.length > 0 ? args[0] : "Pranav";
-
-        // Print greeting message
-        System.out.println("Hello, " + name + "!");
+    public static void main (String [] args)
+    {
+        System.out.println("Hello App Welcomed ");
     }
 }
