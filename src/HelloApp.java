ddd66dfe42684304044c5a3f72be66d89f821ad1
Sai Ganesh <saiganeshindla322@gmail.com>
2026-03-26 09:52:36 +0530

    Update HelloApp.java

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,7 +1,7 @@
 public class HelloApp {
     public static void main(String[] args) {
         // Get the name from command-line argument
-        String name = args.length > 0 ? args[0] : "Pranav";
+        String name = args.length > 0 ? args[0] : "Sai Ganesh";
 
         // Print greeting message
         System.out.println("Hello, " + name + "!");
